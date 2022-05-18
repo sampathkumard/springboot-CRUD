aad69d9c10c4f62f8fc54e596ac18f7dd4f8610e
sampath <donthulasampathkumar72@gmail.com>
2022-05-18 00:09:03 -0600

    Initial Commit

---
+++ src/test/java/com/sampath/springbootCRUD/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampath.springbootCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
